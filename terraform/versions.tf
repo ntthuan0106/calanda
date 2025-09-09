8fde83e4a90876263c5489b7134dd8fe70df34d4
ntthuan0106 <ntthuan0106.job@gmail.com>
2025-09-09 15:22:16 +0700

    Add S3 bucket

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,8 +1,14 @@
 terraform {
+  backend "s3" {
+    bucket = "thuan-nguyen"
+    region = "ap-southeast-1"
+    encrypt = true
+    key = "tf/terraform.tfstate"
+  }
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.97.0"
+      version = "5.97.0"  
     }
   }
 }
